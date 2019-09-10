319c02c6845f78e4a63d0e637b79cb946b0ad384
Jack Davies <jackethdavies@hotmail.com>
2019-09-10 10:30:15 +0100

    LoginScreen layout done

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import Hero from "../components/Hero.js";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { Input } from "react-native-elements";
 
 export default class LoginScreen extends Component {
   static navigationOptions = {
@@ -11,11 +12,43 @@ export default class LoginScreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View>
+      <View style={styles.mainContainer}>
         <Hero message="Login to FoodMiles!" />
+
+        <View style={styles.loginContainer}>
+          <Input placeholder="Your username" />
+          <Input placeholder="Your password" />
+
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.white}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  loginButton: {
+    backgroundColor: "#388E3C",
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: 10,
+    alignSelf: "stretch",
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10
+  },
+  white: {
+    color: "#FFFFFF",
+    textAlign: "center"
+  }
+});
