78a69a480d2e0e8823684bfa6458486137c3ab46
Gabor <tgabri@gmail.com>
2019-09-13 09:05:16 +0100

    consolelog test

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import Hero from "../components/Hero.js";
-import { Input } from "react-native-elements";
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Hero from '../components/Hero.js';
+import { Input } from 'react-native-elements';
 
-import styles from "../styles/main";
-import font from "../styles/font";
+import styles from '../styles/main';
+import font from '../styles/font';
 
 export default class LoginScreen extends Component {
   static navigationOptions = {
-    title: "Register for FoodMiles"
+    title: 'Register for FoodMiles'
   };
 
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Hero message="Register for FoodMiles!" icon="user" />
+        <Hero message='Register for FoodMiles!' icon='user' />
 
         <View style={regStyles.loginContainer}>
-          <Input placeholder="Your username" />
-          <Input placeholder="Your password" />
-
+          <Input placeholder='Your username' />
+          <Input placeholder='Your password' />
+          {console.log('test')}
           <TouchableOpacity style={regStyles.registerButton}>
             <Text style={[font.white, font.center]}>Register</Text>
           </TouchableOpacity>
@@ -32,15 +32,15 @@ export default class LoginScreen extends Component {
 const regStyles = StyleSheet.create({
   loginContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   registerButton: {
-    backgroundColor: "#388E3C",
+    backgroundColor: '#388E3C',
     padding: 10,
     borderRadius: 4,
     marginBottom: 10,
-    alignSelf: "stretch",
+    alignSelf: 'stretch',
     marginLeft: 10,
     marginRight: 10,
     marginTop: 10
